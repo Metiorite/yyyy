3f7c9595c3d4f2129e3aba82f6306c57321837a3
comrade09 <106185020+comrade09@users.noreply.github.com>
2022-12-04 21:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
 os.system("git clone https://comrade09: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@github.com/username/yyyy ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m Telegram main.py &")
